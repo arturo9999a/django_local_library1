dcc20a8e99589e00f6a58f2468391e687ce643b7
arturo9999a <56418813+arturo9999a@users.noreply.github.com>
2020-03-16 14:54:09 -0600

    Update and rename locallibrary/settings.py to  locallibrary/settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -130,3 +130,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+
+
