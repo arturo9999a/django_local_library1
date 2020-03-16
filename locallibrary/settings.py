9d12bb262154e4e132584fde7b4b6a4bce2b6674
arturo9999a <56418813+arturo9999a@users.noreply.github.com>
2020-03-16 14:52:59 -0600

    Update and rename locallibrary/settings.py to  locallibrary/settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -115,12 +115,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -130,3 +124,9 @@ LOGIN_REDIRECT_URL = '/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
